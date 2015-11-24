9da4863917bc7bb2eeceace217ff0222748d212e
DrScott <dougscott2@gmail.com>
2015-11-24 17:02:00 -0500

    initial comment

---
+++ src/purchase_clojure/core.clj
@@ -0,0 +1,27 @@
+(ns purchase-clojure.core
+  (:require [clojure.string :as str]
+            [clojure.walk :as walk])
+  (:gen-class))
+
+(defn -main [& args]
+  (println "what do you want to sort by?")
+  (let [purchases (slurp "purchases.csv")         ;slurp the file
+        purchases (str/split-lines purchases)
+        purchases (map (fn [line]
+                         (str/split line #","))
+                       purchases)
+        header (first purchases)
+        purchases (rest purchases)
+        purchases (map (fn [line]
+                         (interleave header line))
+                       purchases)
+        purchases (map (fn [line]
+                         (apply hash-map line))
+                       purchases)
+        purchases (walk/keywordize-keys purchases)
+        input (read-line)
+        purchases (filter (fn [line]
+                            (= input (:category line)))
+                          purchases)]
+    (spit "filtered_purchases.edn"
+          (pr-str purchases))))
