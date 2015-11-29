5642839a863b9d8e4a350deaa87d439a33243bc2
DrScott <dougscott2@gmail.com>
2015-11-29 11:34:36 -0500

    included ring and hiccup to display onto html page

--- src/purchase_clojure/core.clj
+++ src/purchase_clojure/core.clj
@@ -1,11 +1,12 @@
 (ns purchase-clojure.core
   (:require [clojure.string :as str]
             [clojure.walk :as walk]
-            [clojure.pprint :as pp])
+            [clojure.pprint :as pp]
+            [ring.adapter.jetty :as j]
+            [hiccup.core :as h])
   (:gen-class))
 
-(defn -main [& args]
-  (println "what do you want to sort by?")
+(defn read-purchase []
   (let [purchases (slurp "purchases.csv")         ;slurp the file
         purchases (str/split-lines purchases)
         purchases (map (fn [line]
@@ -20,9 +21,40 @@
                          (apply hash-map line))
                        purchases)
         purchases (walk/keywordize-keys purchases)
-        input (read-line)
-        purchases (filter (fn [line]
+         input (read-line)
+            purchases (filter (fn [line]
                             (= input (:category line)))
                           purchases)]
-    (spit "filtered_purchases.edn"
-          (with-out-str (pp/pprint purchases)))))
+   #_ (spit "filtered_purchases.edn"
+          (with-out-str (pp/pprint purchases)))
+   purchases))
+
+(defn purchase-html []
+  (let [purchases (read-purchase)]
+    (map (fn [line]
+           [:p (str (:customer_id line)
+                    " "
+                    (:date line)
+                    ""
+                    (:credit_card line)
+                    ""
+                    (:cvv line)
+                    ""
+                    (:category line))])
+         purchases))
+  )
+
+(defn handler [request]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    (h/html [:html
+                     [:body
+                      [:br]
+                      (purchase-html)
+                      ]])}
+  )
+
+(defn -main [& args]
+  (j/run-jetty #'handler {:port 3000 :join? false})
+
+  )
